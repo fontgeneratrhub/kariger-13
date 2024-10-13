d2b9e584d840912a24d3a1025c2b874c645240c9
fontgeneratrhub <fontgeneratorh@gmail.com>
2024-10-13 13:36:20 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'public', // Set the root directory to public
+  root: 'public', // This should be set to the public folder
   build: {
-    outDir: '../dist', // Build output directory
+    outDir: '../dist', // Specify where to output the build files
     emptyOutDir: true, // Clear out the output directory before build
   },
 });
