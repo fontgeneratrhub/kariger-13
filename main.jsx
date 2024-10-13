326a19c8bd6f670bc149a8b267a5d938effe7581
fontgeneratrhub <fontgeneratorh@gmail.com>
2024-10-13 13:15:22 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,18 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
-import "./index.css";
-import store from "./redux/store";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
+your-project/
+├── public/
+│   ├── index.html
+├── src/
+│   ├── main.jsx      // Ensure this file exists
+│   └── other-components/
+├── package.json
+└── vite.config.js
